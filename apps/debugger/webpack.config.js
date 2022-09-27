6d349867c289e67f9cc67d44589a69ea8d23231a
yann300 <yann.levreau@gmail.com>
2022-09-27 09:45:11 +0200

    simulator in a web worker

--- apps/debugger/webpack.config.js
+++ apps/debugger/webpack.config.js
@@ -1,10 +1,19 @@
 const nxWebpack = require('@nrwl/react/plugins/webpack')
 const TerserPlugin = require('terser-webpack-plugin')
+const path = require('path')
 
 module.exports = config => {
   const nxWebpackConfig = nxWebpack(config)
+  console.log(nxWebpackConfig)
+
   const webpackConfig = {
     ...nxWebpackConfig,
+    resolve: {
+      ...nxWebpackConfig.resolve,
+      alias: {
+        path: require.resolve("path-browserify"),
+      }
+    },
     node: {
       fs: 'empty',
       tls: 'empty',
@@ -14,6 +23,29 @@ module.exports = config => {
       child_process: 'empty'
     }
   }
+
+  webpackConfig.module.rules.push({
+    test: /\.js|jsx$/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: [
+                ["@babel/preset-env", {
+                    useBuiltIns: 'entry',
+                    corejs: 3
+                }],
+                "@babel/preset-react"
+            ],
+            plugins: [
+                
+            ],
+            include: [
+                path.resolve('node_modules/ethereumjs/'),
+            ],
+            exclude: /node_modules\/(?!ethereumjs).+/
+        }
+    }
+})
 
   if (process.env.NODE_ENV === 'production') {
     return {
